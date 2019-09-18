810a058ef93ae21805812bc361692880c029e2d7
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-09-19 00:22:05 +0200

    c low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int longlongType;
 	float floatType;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
-	printf("Size of a long long int: %lld byte(s)\n", sizeof(longlongType));
-	printf("Size of a float: %f bytes\n", sizeof(floatType));
+	printf("Size of a char: %i byte(s)\n", sizeof(charType));
+	printf("Size of an int: %i byte(s)\n", sizeof(integerType));
+	printf("Size of a long int: %i byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %i bytes\n", sizeof(floatType));
 	return (0);
 }
