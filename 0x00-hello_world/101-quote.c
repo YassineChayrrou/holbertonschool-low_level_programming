bbfb55567cf4ddca63baa12bb0514c4913ee5d2a
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-09-19 00:56:13 +0200

    c low level programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * return
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int fd;
+
+	write(fd, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+}
