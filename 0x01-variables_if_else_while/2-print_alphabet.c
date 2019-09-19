79cd308dd6a8c7487f4540894260e0a8bc5807b2
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-09-19 13:40:09 +0200

    c programming variables and if, else, while statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[27] = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(c);
+	putchar(\n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char al[27] = "abcdefghijklmnopqrstuvwxyz";
+	char au[27] = "BCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	putchar(al);
+	putchar(au);
+	putchar(\n);
+	return (0);
+}
