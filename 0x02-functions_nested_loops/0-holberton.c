d9912c88b525fcfb6a81563a88d7db459d6d53ea
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-09-24 10:48:41 +0200

    Funcitons, netsted loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *Program that prints Holberton
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
