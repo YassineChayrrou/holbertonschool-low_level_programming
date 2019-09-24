fc61157c37e985105f442a71615c6090dfcab56f
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-09-24 15:53:12 +0200

    fix alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * print_alphabet()
- *prints alphabets from a to z
- * Return: void
+ * print_alphabet - entry point
+ * print alphabet
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
