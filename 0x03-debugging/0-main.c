29ab41531ed844b13b9f3d532df2bbaa35b48f9e
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-09-24 23:39:43 +0200

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-  *
-  *
-  *
-  */
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	positive_or_negative(0);
