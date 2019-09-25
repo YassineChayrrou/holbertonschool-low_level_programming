85777231a9b9f18c2fe73293b481514f35dfb348
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-09-25 22:01:26 +0000

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+   * add - add two numbers
+    * @a: number
+     * @b: number
+      * Return: Always 0 (Success)
+       */
+int add(int a, int b)
+{
+	return (a + b);
+}
