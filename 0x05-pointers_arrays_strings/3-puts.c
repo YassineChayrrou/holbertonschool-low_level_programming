da9e3169f321dca34c0ad6ca8d54178daee85f0a
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-01 11:52:01 +0200

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  *_puts - prints a string followed by new line to standard output
+  *@str: points to str
+  *Return: Always 0
+  */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
