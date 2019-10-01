0e28930f00a34c5e8a64380eb71d0eff4e3a36f2
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-01 12:05:54 +0200

    Hlib

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,9 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+
 #endif /* _HOLBERTON_H */
