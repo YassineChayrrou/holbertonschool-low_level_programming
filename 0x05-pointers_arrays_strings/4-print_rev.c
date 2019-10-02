a17af9889f5030c036c3fba0fbb0dc9f2ea4e169
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-02 08:13:25 +0200

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
+
 /**
-  *print_rev -  prints string in reverse
-  *@s: points to s
-  *Return: Always 0
-  */
+ * print_rev - print a string in reverse
+ * @s: char array string type
+ */
+
 void print_rev(char *s)
 {
 	int i;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	for (i = i; s[i] >=  0; i--)
-	{
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i--; i >= 0; i--)
 		_putchar(s[i]);
-	}
+
 	_putchar('\n');
 }
