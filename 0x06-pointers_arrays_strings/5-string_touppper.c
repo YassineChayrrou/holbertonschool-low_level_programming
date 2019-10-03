7d38e0b6bdd163a4677b3bb3c2b219d7db054918
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-04 00:16:11 +0200

    fix

--- 0x06-pointers_arrays_strings/5-string_touppper.c
+++ 0x06-pointers_arrays_strings/5-string_touppper.c
@@ -1,19 +1,18 @@
 #include "holberton.h"
 /**
- * string_toupper - Change all lowercase letters to uppercase
- * @c: string
- * Return: c
- */
-char *string_toupper(char *c)
+* string_toupper - copie n bytes of a string
+* @s: points to string
+* Return: s
+*/
+char *string_toupper(char *s)
 {
-	int i;
-
-	for (i = 0; c[i] != '\0'; i++)
-	{
-		if (c[i] > 96 && c[i] < 123)
-		{
-			c[i] -= 32;
-		}
-	}
-	return (c);
+int i = 0, j;
+while (s[i])
+i++;
+for (j = 0; j < i; j++)
+{
+if (s[j] >= 97 && s[j] <= 122)
+s[j] -= 32;
+}
+return (s);
 }
