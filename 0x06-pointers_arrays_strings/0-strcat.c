305ecde89d745d128173e12ff125cfeab0ad061c
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-03 13:18:16 +0200

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  *_strcat - concatenate two strings
+  *@dest: points to dest
+  *@src: points to src
+  *Return: nothing
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j  = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] ='\0';
+}
