58a908a8aec66ff7100095639848ccfd99c5735e
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-03 13:34:30 +0200

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,25 @@
 #include "holberton.h"
+
 /**
-  *_strcat - concatenate two strings
-  *@dest: points to dest
-  *@src: points to src
-  *Return: nothing
-  */
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
+ */
+
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i, c;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-	for (j  = 0; src[j] != '\0'; j++, i++)
+
+	for (c = 0; src[c] != '\0'; c++)
 	{
-		dest[i] = src[j];
+		dest[i] = src[c];
+		i++;
 	}
-	dest[i] ='\0';
+
+	dest[i] = '\0';
+	return (dest);
 }
