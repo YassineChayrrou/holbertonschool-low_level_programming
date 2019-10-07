3db7ef552289bbaac0c0ebc497127094060a698f
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-08 01:00:07 +0200

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,30 +1,32 @@
-#include "holblerton.h"
+#include "holberton.h"
+#include <stdlib.h>
 /**
-  *_strpbrk - searches a string for any of a set of bytes
-  *@s: points to string
-  *@accept: string to match with s
-  *Return: pointer to the byte in s or NULL if no bytes are found
-  */
-char *_strpbrk(char *s, char *accept)
+ * _strstr - locate a substring
+ * @haystack: string
+ * @needle: string substring
+ * Return: Pointer to beginning of substring, or NULL if not found.
+ */
+char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-	char *p;
+	int i, j, c;
 
 	i = 0;
-	while (s[i] != '\0')
+	c = 0;
+	while (haystack[i] != '\0')
 	{
 		j = 0;
-		while (accept[j] != '\0')
+		while (needle[j + c] != '\0' && haystack[i + c] != '\0'
+		       && needle[j + c] == haystack[i + c])
 		{
-			if (accept[j] == s[i])
-			{
-				p = &s[i];
-				return (p);
-			}
-			j++;
+			if (haystack[i + c] != needle[j + c])
+				break;
+			c++;
 		}
+		if (needle[j + c] == '\0')
+			return (&haystack[i]);
+		j++;
 		i++;
 	}
 
-	return (0);
+	return (NULL);
 }
