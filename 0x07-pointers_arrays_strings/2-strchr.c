ee271f85399e0381e3c5b3901470e3543f4748f9
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-07 22:31:36 +0200

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  *_strchr - locates a character in a string
+  *@s: points to string
+  *@c: character to locate
+  *Return: s
+  */
+char *_strchr(char *s, char c)
+{
+	do 
+	{
+		if (*s == c)
+		{
+			return ((char*)s);
+		}
+	}
+	while (*s++);
+	return (0);
+}
