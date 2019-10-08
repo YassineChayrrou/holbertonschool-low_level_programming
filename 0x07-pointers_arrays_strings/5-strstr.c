03d99174d581d0d0ed7e74c7ba8243fcdd1190b6
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-08 06:26:05 +0200

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+  *_strstr - locates a substring
+  *@haystack: string
+  *@needle: occurance string to locate
+  *Return:or NULL
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	
+	i = 0;
+	j = 0;
+	do {
+		
+		while (haystack[i] == needle[j])
+		{
+			j++;
+		}
+		return (needle);
+	} while (i++);
+	return (0);
+}
