bffc684783617ec912733db578d5131708d4d1f0
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-13 21:04:21 +0200

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+  *main - function that adds integer arguments
+  *@argc: argument count
+  *@argv: argument vector
+  */
+int main(int argc, char *argv[])
+{
+	int i, j, Result;
+
+	Result = 0;
+	if (argc == 0)
+		printf("0 \n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error \n");
+				return (1);
+			}
+		}
+		Result += atoi(argv[i]);
+	}
+	printf("%d \n", Result);
+	}
+}
