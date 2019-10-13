ab2409f0178e870b36962d18d7d9f4c797adee12
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-13 21:34:44 +0200

    fix it again

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] >= 48 && argv[i][j] <= 57)
+			if (isalpha(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
