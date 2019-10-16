710804155394e0f12d2ff6aab02ff21a70b01f31
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-16 22:10:25 +0200

    fix again

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,20 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-  * _strlen - find string length
-  * @s: takes in string
-  * Return: i
-  *
-  */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-/**
   *str_concat - concat to string and store them in allocated memory space
   *@s1: takes a string
   *@s2: takes a string
@@ -23,13 +9,15 @@ int _strlen(char *s)
   */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, l1, l2;
+	int i, l1, l2;
 	char *A;
 
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	for (l1 = 0; s1[l1] != '\0'; l1++)
+		;
+	for (l2 = 0; s2[l2] != '\0'; l2++)
+		;
 	A = malloc((l1 + l2 + 1) * sizeof(char));
 	if (A == NULL)
 		return (NULL);
