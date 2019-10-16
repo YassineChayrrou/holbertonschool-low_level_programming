7d44c1c0efc73fea4ba43950b162144a6f319038
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-16 22:22:53 +0200

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,12 +12,16 @@ char *str_concat(char *s1, char *s2)
 	int i, l1, l2;
 	char *A;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	for (l1 = 0; s1[l1] != '\0'; l1++)
-		;
-	for (l2 = 0; s2[l2] != '\0'; l2++)
-		;
+	if (s1 == NULL)
+		l1 = 0;
+	else
+		for (l1 = 0; s1[l1] != '\0'; l1++)
+			;
+	if (s2 == NULL)
+		l2 = 0;
+	else
+		for (l2 = 0; s2[l2] != '\0'; l2++)
+			;
 	A = malloc((l1 + l2 + 1) * sizeof(char));
 	if (A == NULL)
 		return (NULL);
