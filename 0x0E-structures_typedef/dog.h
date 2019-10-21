fc20cedae0c4da9c369a61373bda41d7d8d5d7fd
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-22 00:47:33 +0200

    com

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,25 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - first struct
- * @name: First member
- * @age: Second member
- *@owner: Third member
- */
+  * struct dog - dog structure
+  * @name: first mem
+  * @age: second mem
+  * @owner: third mem
+  *
+  * Description: dog structure with 3 members
+  *
+  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-typedef struct dog dog_t;
-
-#endif
+#endif /* DOG_H */
