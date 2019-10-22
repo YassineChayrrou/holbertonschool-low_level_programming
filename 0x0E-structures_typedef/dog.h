aa0f1560e95ff85ba95f1acdc4cdfdfe5766d5c5
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-23 00:37:12 +0200

    halo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,16 +10,15 @@
   * Description: dog structure with 3 members
   *
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-typedef struct dog dog_t;
+} dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* DOG_H */
+#endif
