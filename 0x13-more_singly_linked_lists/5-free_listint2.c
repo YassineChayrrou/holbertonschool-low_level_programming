df4adfb0ba4f0544c1a41efb64e24135df4a80bc
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-11-05 21:53:03 +0100

    fixing head==NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
-  *
-  *
-  *
+  *free_listint2 - free LL2
+  *@head: double pointer
   */
 void free_listint2(listint_t **head)
 {
 	listint_t *to_free;
 
+	if (head == NULL)
+		return;
 	while (head != NULL)
 	{
 		to_free = *head;
 		free(to_free);
-		to_free->next;
+		(*head)->->next;
 	}
-	*head = NULL;
 }
