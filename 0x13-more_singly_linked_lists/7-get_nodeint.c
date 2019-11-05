1d8847dd6600c8903869a8b116332b5e0a4ead14
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-11-05 23:14:25 +0100

    fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; i < index; i++)
 	{
-		if (head == NULL)
+		if (head->next == NULL)
 			return (NULL);
 		head = head->next;
 		return (head);
