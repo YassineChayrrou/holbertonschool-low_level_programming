46cbf35cc1e0e202ab46b8f2d6df7cbfb70fbde2
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-11-05 21:30:08 +0100

    free LL

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+  *free_listint - free LL
+  *@head: head position in LL
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *to_free;
+
+	while (head != NULL)
+	{
+		to_free = head;
+		to_free->next;
+		free(head);
+	}
+}
