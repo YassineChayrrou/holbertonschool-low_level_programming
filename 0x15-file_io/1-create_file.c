a1202060e4450cd7dcea307871c27b54c316e524
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-11-16 21:01:37 +0000

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+  *create_file - creates a file
+  *@filename: filename
+  *@text_content: pointer to char
+  *Return: 1 or -1
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fileDescriptor, len, writeContent;
+
+	if (filename == NULL)
+		return (-1);
+	fileDescriptor = open(filename, O_CREAT | O_TRUNC | O_WRONLY | O_RDONLY, 600);
+	if (fileDescriptor == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+	writeContent = write(fileDescriptor, text_content, len);
+	if (writeContent == -1)
+	{
+		return (-1);
+	}
+	close(fileDescriptor);
+	return (1);
+}
