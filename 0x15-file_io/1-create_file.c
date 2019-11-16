19a12fa41c7f35b74e7f2443176617bfc83a390b
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-11-16 21:09:10 +0000

    ddd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fileDescriptor = open(filename, O_CREAT | O_TRUNC | O_WRONLY | O_RDONLY, 600);
+	fileDescriptor = open(filename, O_CREAT | O_TRUNC | O_RDWR, 600);
 	if (fileDescriptor == -1)
 		return (-1);
 	if (text_content == NULL)
