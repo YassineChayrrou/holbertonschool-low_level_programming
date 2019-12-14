8725a2ffed47e91dd7cae20852a9ac2dc1a5ff4f
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-12-14 20:35:02 +0000

    betty issue fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 /**
-  *
-  *
-  */
+  *print_dlistint - prints a linked list
+  *@h: pointer to first node of a linked list
+  *Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
+
 	for (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
